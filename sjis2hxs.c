87751443d644c076c1c05658e89696d8967c1cca
roris <roris@users.noreply.github.com>
2017-10-23 22:08:50 +0530

    License under WTFPL

--- sjis2hxs.c
+++ sjis2hxs.c
@@ -1,3 +1,10 @@
+/** 
+ * Copyright © 2017 roris
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
+
 #define WIN32_LEAN_AND_MEAN
 
 #include <windows.h>
